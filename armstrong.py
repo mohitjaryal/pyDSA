4f097dbfb97b8495cdb351699fa73b29d205425f
Mohit <mohitt.tech@gmail.com>
2025-11-03 21:49:51 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -7,3 +7,10 @@ ln = len(str(n))
 
 while(num>0):
     ld = num % 10
+    total = total + (ld ** ln) # adding last digit to the total
+    num //= 10
+
+if(total == n ):
+    print('Yes it is an Armstrong number')
+else:
+    print("No it's not an armstrong number ")
