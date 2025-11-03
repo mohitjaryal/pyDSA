03c846e2163bbd4c3ecd6d67a7da460a27141f57
Mohit <mohitt.tech@gmail.com>
2025-11-03 22:00:41 +0530

    Update checkDivisor.py

--- BruteForce/checkDivisor.py
+++ BruteForce/checkDivisor.py
@@ -1,1 +1,9 @@
 # Display divisor of number using 'Brute Force Approach'
+n = int(input('Enter number :'))
+
+num = n
+result = []
+
+for i in range(1,num+1):
+    if(num % 2 == 0):
+        result.append(i) # adding divisor to the result
