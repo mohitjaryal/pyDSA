5bb0686f46701adeea2257fccc17c047706dfc84
Mohit <mohitt.tech@gmail.com>
2025-11-03 21:24:54 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,7 @@
 # if the number is palindrome return true otherwise flase
+n = int(input('Enter number :'))
+num = n 
+result = 0
+while(num>0):
+    last_digit = num % 2
+    result = (result * 10) + last_digit
