4bd75adb99efdd487a715ec84b5eb2393bdd5214
Mohit <mohitt.tech@gmail.com>
2025-11-03 20:53:36 +0530

    Update extractNums.py

--- extractNums.py
+++ extractNums.py
@@ -1,8 +1,10 @@
 # Extract numbers
 # User input
 n = int(input('Enter number :'))
-
+print('You entered :',n)
 num = n
 
 # Main logic
 while (num > 0):
+    last_digit = num % 10 # Taking last digit
+    print(last_digit)
