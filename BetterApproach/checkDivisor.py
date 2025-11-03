4d12ab89ef8fcc41790cc499dde907ab5c8a5edf
Mohit <mohitt.tech@gmail.com>
2025-11-03 22:12:06 +0530

    Update checkDivisor.py

--- BetterApproach/checkDivisor.py
+++ BetterApproach/checkDivisor.py
@@ -1,1 +1,8 @@
 # Display divisor of number using 'Better Approach'
+
+num = int(input('Enter a number :'))
+n = num
+result = [] # Empty list
+for i in range(n // 2):
+    if(n % i == 0):
+        result.append(i)
