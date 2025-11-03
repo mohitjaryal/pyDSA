0c605dd908c9739732007eef74b99512443dc41e
Mohit <mohitt.tech@gmail.com>
2025-11-03 21:28:11 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,5 +3,10 @@ n = int(input('Enter number :'))
 num = n 
 result = 0
 while(num>0):
-    last_digit = num % 2
+    last_digit = num % 10
     result = (result * 10) + last_digit
+    num //= 10
+if n == result:
+    print(True)
+else:
+    print(False)
