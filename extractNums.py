9a9c4106fbbf9a2b77cadf59fedf1d4550fdf492
Mohit <mohitt.tech@gmail.com>
2025-12-16 23:59:44 +0530

    Update extractNums.py

---
+++ extractNums.py
@@ -0,0 +1,11 @@
+# Extract numbers
+# User input
+n = int(input('Enter number :'))
+print('You entered :',n)
+num = n
+
+# Main logic
+while (num > 0):
+    last_digit = num % 10 # Taking last digit
+    print(last_digit)
+    num //= 10 # just taking integer val only not decimal
